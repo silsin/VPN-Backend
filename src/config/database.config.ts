ac33d8701c1786f0175e6e272bf38b4f83850856
hasan <hasan@digilite.ca>
2025-11-26 10:24:00 +0330

    config db

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -9,13 +9,13 @@ export class DatabaseConfig implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: this.configService.get<string>('DB_HOST', 'localhost'),
+      host: this.configService.get<string>('DB_HOST', '195.181.173.71'),
       port: this.configService.get<number>('DB_PORT', 5432),
-      username: this.configService.get<string>('DB_USERNAME', 'postgres'),
-      password: this.configService.get<string>('DB_PASSWORD', 'postgres'),
+      username: this.configService.get<string>('DB_USERNAME', 'myuser2347'),
+      password: this.configService.get<string>('DB_PASSWORD', 'gfx234789!!!@@@###'),
       database: this.configService.get<string>('DB_NAME', 'flyvpn'),
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: this.configService.get<string>('NODE_ENV') !== 'production',
+      synchronize: false, // Disabled - using manual SQL migrations
       logging: this.configService.get<string>('NODE_ENV') === 'development',
       ssl: this.configService.get<string>('DB_SSL') === 'true' ? { rejectUnauthorized: false } : false,
     };
