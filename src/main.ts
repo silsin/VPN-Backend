3ea9d590f6e07c9dc060b4905099dd7cb8ef0877
hasan <hasan@digilite.ca>
2025-11-27 11:39:02 +0330

    new changes

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port,'0.0.0.0');
   console.log(`Application is running on: http://localhost:${port}`);
   console.log(`Swagger documentation: http://localhost:${port}/api/docs`);
 }
