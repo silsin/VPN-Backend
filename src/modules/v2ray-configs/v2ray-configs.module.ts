4d0eff1b610c7bcc9d81b8508b5a7598e8968305
hasan <hasan@digilite.ca>
2025-12-21 18:23:58 +0330

    fix build issue

--- src/modules/v2ray-configs/v2ray-configs.module.ts
+++ src/modules/v2ray-configs/v2ray-configs.module.ts
@@ -5,9 +5,10 @@ import { V2RayConfigsController } from './v2ray-configs.controller';
 import { V2RayConfig } from './entities/v2ray-config.entity';
 import { HandshakeModule } from '../handshake/handshake.module';
 import { V2RayConfController } from './v2ray-conf.controller';
+import { DeviceSession } from '../handshake/entities/device-session.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([V2RayConfig]), HandshakeModule],
+  imports: [TypeOrmModule.forFeature([V2RayConfig, DeviceSession]), HandshakeModule],
   controllers: [V2RayConfigsController, V2RayConfController],
   providers: [V2RayConfigsService],
 })
